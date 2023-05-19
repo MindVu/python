a83f0d4ba3c61de19643cfb165aef0dfe2f877e6
MindVu <minhducvu1311@gmail.com>
2023-05-19 12:30:11 +0700

    updt

---
+++ AIproject/main.py
@@ -0,0 +1,25 @@
+import instaloader
+import csv
+
+username = "joe_mama2833"
+password = "mothaiba"
+link = input("Search link: ")
+link.split("/")[-2]
+L = instaloader.Instaloader()
+
+L.context.login(username, password)
+
+post = instaloader.Post.from_shortcode(L.context, shortcode=link.split("/")[-2])
+
+comments = post.get_comments()
+
+# Save comments to a CSV file
+with open('comments.csv', 'w', newline='', encoding='utf-8-sig') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Comment'])
+    counter = 0
+    for comment in comments:
+        if counter >= 5000:
+            break
+        writer.writerow([comment.text])
+        counter += 1
